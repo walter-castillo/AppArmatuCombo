868ee057ff32a4f356570662b057dc28f43ecce4
Walter Castillo <waltercastillo@live.com.ar>
2025-07-21 14:19:51 -0300

    agregue botoenes home

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 
 export default function Home() {
   return (
@@ -5,7 +7,10 @@ export default function Home() {
       <h1 className="text-3xl font-bold mb-4">Bienvenido a la App de Compra de Hamburguesas</h1>
       <p className="text-lg text-gray-700 mb-2">Elige y compra tus hamburguesas favoritas de manera fácil y rápida.</p>
       <p className="text-base text-gray-500">Disfruta de una experiencia sencilla, rápida y segura. ¡Explora nuestro menú y haz tu pedido ahora!</p>
-      <button className="mt-6 px-6 py-2 bg-yellow-500 text-white rounded-lg shadow hover:bg-yellow-600 transition">Ver menú</button>
+      <div className="mt-6 flex gap-4">
+        <Link href="/bases" className="px-6 py-2 bg-yellow-500 text-white rounded-lg shadow hover:bg-yellow-600 transition">Ver bases</Link>
+        <Link href="/combo" className="px-6 py-2 bg-yellow-500 text-white rounded-lg shadow hover:bg-yellow-600 transition">Ver combos</Link>
+      </div>
     </div>
   );
 }
--- store/selectors/comboSelectors.js
+++ store/selectors/comboSelectors.js
@@ -1,11 +1,11 @@
-export const selectBases = (state) => state.combo.bases;
-export const selectIngredients = (state) => state.combo.ingredients;
-export const selectDrinks = (state) => state.combo.drinks;
+export const selectBases = (state) => state.combo2.bases;
+export const selectIngredients = (state) => state.combo2.ingredients;
+export const selectDrinks = (state) => state.combo2.drinks;
 
-export const selectSelectedCombo = (state) => state.combo.selectedCombo;
-export const selectSelectedBase = (state) => state.combo.selectedCombo.base;
-export const selectSelectedIngredients = (state) => state.combo.selectedCombo.ingredients;
-export const selectSelectedDrink = (state) => state.combo.selectedCombo.drink;
+export const selectSelectedCombo = (state) => state.combo2.selectedCombo;
+export const selectSelectedBase = (state) => state.combo2.selectedCombo.base;
+export const selectSelectedIngredients = (state) => state.combo2.selectedCombo.ingredients;
+export const selectSelectedDrink = (state) => state.combo2.selectedCombo.drink;
 
-export const selectTotalPrice = (state) => state.combo.selectedCombo.totalPrice;
-export const selectTotalCal = (state) => state.combo.selectedCombo.totalCal;
+export const selectTotalPrice = (state) => state.combo2.selectedCombo.totalPrice;
+export const selectTotalCal = (state) => state.combo2.selectedCombo.totalCal;
