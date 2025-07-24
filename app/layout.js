b5ea7321d18a525cdbdf6b027db4f4e135657194
Walter Castillo <waltercastillo@live.com.ar>
2025-07-24 14:28:57 -0300

    etiqueta meta

--- app/layout.js
+++ app/layout.js
@@ -5,12 +5,6 @@ import "./globals.css";
 
 
 
-
-export const metadata = {
-  title: "Armar Combo",
-  description: "arma tu combo personalizado",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
