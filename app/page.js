bab53284942071ab9d560cab27b9c25a59d6defd
Walter Castillo <waltercastillo@live.com.ar>
2025-07-25 11:13:40 -0300

    agregue metadata a page principal

--- app/page.js
+++ app/page.js
@@ -1,5 +1,34 @@
 import Link from "next/link";
 
+export const metadata = {
+  title: "ArmaTuCombo | El mejor lugar para crear tu hamburguesa ideal",
+  description:
+    "Bienvenido a ArmaTuCombo, la app donde podés armar tu hamburguesa y combo perfecto paso a paso. Ingredientes frescos, combinaciones únicas y todo a tu gusto.",
+  openGraph: {
+    title: "ArmaTuCombo | Crea tu combo perfecto",
+    description:
+      "Elegí entre hamburguesas, ingredientes, bebidas y más. Armar tu combo nunca fue tan fácil y divertido.",
+    url: "https://app-armatu-combo.vercel.app",
+    siteName: "ArmaTuCombo",
+    images: [
+      {
+        url: "https://app-armatu-combo.vercel.app/bases/doble.png", // Imagen atractiva del producto o logo
+        width: 1200,
+        height: 630,
+        alt: "ArmaTuCombo - El mejor combo a tu medida",
+      },
+    ],
+    locale: "es_AR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "ArmaTuCombo | Crea tu combo perfecto",
+    description:
+      "Arma tu hamburguesa y combo ideal con ingredientes frescos y opciones personalizadas. ¡Arrancá ahora!",
+    images: ["https://app-armatu-combo.vercel.app/bases/doble.png"],
+  },
+};
 
 export default function Home() {
   return (
